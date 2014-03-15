667bf91bb491e593e2d20cfacc5eaeeacacbc2c5
Kory Nunn <knunn187@gmail.com>
2014-03-15 14:25:48 +1000

    Update retorter.js

--- server/router/retorter.js
+++ server/router/retorter.js
@@ -1,24 +1,24 @@
 var Retort = require('retort'),
     retorter = new Retort({
         ok: function(request, response, data, isNotJson){
-            response.writeHead(200);
+            response.statusCode = 200;
             response.end(isNotJson ? data + '' : JSON.stringify(data));
         },
         forbidden: function(request, response, message, isNotJson){
-            response.writeHead(403, isNotJson ? message + '' : JSON.stringify(message));
-            response.end();
+            response.statusCode = 403;
+            response.end(isNotJson ? message + '' : JSON.stringify(message));
         },
         unauthorised: function(request, response, message, isNotJson){
-            response.writeHead(301, isNotJson ? message + '' : JSON.stringify(message));
-            response.end();
+            response.statusCode = 301;
+            response.end(isNotJson ? message + '' : JSON.stringify(message));
         },
         error: function(request, response, error, isNotJson){
-            response.writeHead(500);
+            response.statusCode = 500;
             response.end(isNotJson ? error + '' : JSON.stringify(error));
         },
         redirect: function(request, response, location){
-            var redirect = { Location: location };
-            response.writeHead(302, redirect);
+            response.statusCode = 302;
+            response.setHeader('Location', location);
             response.end();
         }
     });
